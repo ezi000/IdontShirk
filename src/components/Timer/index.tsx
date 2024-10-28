584c02755f99a36304e0d860e85ea243e0855161
Damian Szklarczyk <damian@xtramile.no>
2024-10-28 21:46:23 +0100

    simple timer

---
+++ src/components/Timer/index.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+
+const Timer = () => {
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout | null = null;
+
+    if (isRunning) {
+      interval = setInterval(() => {
+        setSeconds((prevSeconds) => {
+          if (prevSeconds === 59) {
+            setSeconds(0);
+            setMinutes((prevMinutes) => {
+              if (prevMinutes === 59) {
+                setMinutes(0);
+                setHours((prevHours) => prevHours + 1);
+                return 0;
+              } else {
+                return prevMinutes + 1;
+              }
+            });
+            return 0;
+          } else {
+            return prevSeconds + 1;
+          }
+        });
+      }, 1000);
+    } else if (interval) {
+      clearInterval(interval);
+    }
+
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [isRunning]);
+
+  const startTimer = () => setIsRunning(true);
+  const stopTimer = () => setIsRunning(false);
+  const resetTimer = () => {
+    setIsRunning(false);
+    setHours(0);
+    setMinutes(0);
+    setSeconds(0);
+  };
+
+  return (
+    <div>
+      <div>
+        {String(hours).padStart(2, "0")}:{String(minutes).padStart(2, "0")}:
+        {String(seconds).padStart(2, "0")}
+      </div>
+      <button onClick={startTimer}>Start</button>
+      <button onClick={stopTimer}>Stop</button>
+      <button onClick={resetTimer}>Reset</button>
+    </div>
+  );
+};
+
+export default Timer;
--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,7 +1,9 @@
+import Timer from "../../components/Timer";
+
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <Timer />
     </div>
   );
 };
